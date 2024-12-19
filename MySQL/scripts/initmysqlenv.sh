b2d5fa6bb505e0943f463ff3120f5d7afe39deef
mspetroula <104512191+mspetroula@users.noreply.github.com>
2024-12-19 10:41:41 -0500

    Update initmysqlenv.sh

--- MySQL/scripts/initmysqlenv.sh
+++ MySQL/scripts/initmysqlenv.sh
@@ -4,15 +4,14 @@ sudo apt-get -y -qq install mysql-server
 sudo systemctl start mysql
 mkdir /home/mysqladmin/data
 mkdir /home/mysqladmin/percona
-cd percona
-curl -L https://downloads.percona.com/downloads/Percona-XtraBackup-LATEST/Percona-XtraBackup-8.0.35-30/binary/tarball/percona-xtrabackup-8.0.35-30-Linux-x86_64.glibc2.17.tar.gz -o percona-xtrabackup-8.0.35-30-Linux-x86_64.glibc2.17.tar.gz
-gzip -d percona-xtrabackup-8.0.35-30-Linux-x86_64.glibc2.17.tar.gz
-tar -xf percona-xtrabackup-8.0.35-30-Linux-x86_64.glibc2.17.tar
-cd ../data
-curl -L https://downloads.mysql.com/docs/sakila-db.tar.gz -o sakila-db.tar.gz
-gzip -d sakila-db.tar.gz
-tar -xf sakila-db.tar
-cd sakila-db
-sudo mysql -h localhost <sakila-schema.sql
-sudo mysql -h localhost < sakila-data.sql
+curl -Lv https://downloads.percona.com/downloads/Percona-XtraBackup-LATEST/Percona-XtraBackup-8.0.35-30/binary/tarball/percona-xtrabackup-8.0.35-30-Linux-x86_64.glibc2.17.tar.gz -o /home/mysqladmin/percona/percona-xtrabackup-8.0.35-30-Linux-x86_64.glibc2.17.tar.gz --stderr /home/mysqladmin/percona/percona_curl.log
+gzip -d /home/mysqladmin/percona/percona-xtrabackup-8.0.35-30-Linux-x86_64.glibc2.17.tar.gz
+tar -xf /home/mysqladmin/percona/percona-xtrabackup-8.0.35-30-Linux-x86_64.glibc2.17.tar
+cd /home/mysqladmin/data
+curl -Lv https://downloads.mysql.com/docs/sakila-db.tar.gz -o /home/mysqladmin/data/sakila-db.tar.gz --stderr /home/mysqladmin/data/data_curl.log
+gzip -d /home/mysqladmin/data/sakila-db.tar.gz
+tar -xf /home/mysqladmin/data/sakila-db.tar
+cd /home/mysqladmin/data/sakila-db
+sudo mysql -h localhost </home/mysqladmin/data/sakila-db/sakila-schema.sql
+sudo mysql -h localhost </home/mysqladmin/data/sakila-db/sakila-data.sql
 sudo systemctl enable mysql
