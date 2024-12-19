e18cf9eea875c224d514af6361db9f480817c2d0
mspetroula <104512191+mspetroula@users.noreply.github.com>
2024-12-19 17:57:03 -0500

    Update initmysqlenv.sh

--- MySQL/scripts/initmysqlenv.sh
+++ MySQL/scripts/initmysqlenv.sh
@@ -6,7 +6,7 @@ mkdir /home/mysqladmin/data
 mkdir /home/mysqladmin/percona
 cd /home/mysqladmin/percona
 curl -Lv https://downloads.percona.com/downloads/Percona-XtraBackup-LATEST/Percona-XtraBackup-8.0.35-30/binary/tarball/percona-xtrabackup-8.0.35-30-Linux-x86_64.glibc2.17.tar.gz -o percona-xtrabackup-8.0.35-30-Linux-x86_64.glibc2.17.tar.gz --stderr percona_curl.log
-tar -xzvf percona/percona-xtrabackup-8.0.35-30-Linux-x86_64.glibc2.17.tar.gz
+tar -xzvf percona-xtrabackup-8.0.35-30-Linux-x86_64.glibc2.17.tar.gz
 cd /home/mysqladmin/data
 curl -Lv https://downloads.mysql.com/docs/sakila-db.tar.gz -o sakila-db.tar.gz --stderr data_curl.log
 tar -xzvf sakila-db.tar.gz
